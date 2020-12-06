1603109aa36792381a718650557ea2bce195a11e
James <james@jastewart.co.uk>
2020-12-06 16:35:33 +0000

    Comments + some changes

--- mp_twitter_bios/index.js
+++ mp_twitter_bios/index.js
@@ -1,9 +1,15 @@
+// Import an out-the-box Javascript package which allows me
+// to read files on my local computer
 const fs = require('fs')
-var parse = require('csv-parse')
+// Import a package to help me read/write to CSV
+const parse = require('csv-parse')
 
+// Import a package which allows me to launch a Chrome instance
 const puppeteer = require('puppeteer');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+// Create the CSV and key the columns as I'll need to refer
+// to them later
 const csvWriter = createCsvWriter({
     path: 'mp_results1.csv',
     header: [
@@ -14,10 +20,15 @@ const csvWriter = createCsvWriter({
     ]
 });
 
-function contains(target, pattern, party){
+// This function was a double check for me
+function contains(target, pattern){
     let str = [];
+    // Get a paragraph of text, in this cases, the MP's biography
+    // from Twitter, and make it all lowercase.
     const paragraph = target.toLowerCase();
 
+    // Check if it contains any of a group of words that I
+    // pass into this function
     pattern.forEach((word) => {
         if (paragraph.includes(word)) {
             str.push(word);
@@ -59,7 +70,7 @@ const scraper = (async (mp, handle, party) => {
         party,
         has_party: contains(
             bio,
-            ['conservative', 'labour', 'lib dem', 'liberal democrats', 'lib dems', 'snp', 'scottish national party'])
+            ['conservative', 'labour', 'lib dem', '@libdems', 'liberal democrats', 'lib dems', 'snp', 'scottish national party'])
                 .length > 0,
         bio,
     }]);
